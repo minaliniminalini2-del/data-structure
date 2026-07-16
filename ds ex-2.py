93a4ef3576dc62231a2c17eda431e6625d36b2dc
minaliniminalini2-del <minaliniminalini2@gmail.com>
2026-07-16 15:42:24 +0530

    Add files via upload

---
+++ ds ex-2.py
@@ -0,0 +1,6 @@
+range={'1':['a','b'],'2':['c','d'],'3':['e','f'],'4':['g','h']}
+for i in range['1']:
+    for j in range['2']:
+        for k in range['3']:
+            for l in  range['4']:
+              print(i+j+k+l)
